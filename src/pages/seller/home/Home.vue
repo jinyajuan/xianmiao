c57f6b3afc35b2f2845af2157653dde76e934d1c
jinyajuan <2633579745@qq.com>
2019-04-03 16:58:43 +0800

    seller home is finished

--- src/pages/seller/home/Home.vue
+++ src/pages/seller/home/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div>
-    111111111111111
+    <home-header></home-header>
+    <home-today :TodayList="TodayList"></home-today>
+    <home-weekend :WeekendList="WeekendList"></home-weekend>
+    <home-last :LastList="LastList"></home-last>
+    <home-footer></home-footer>
   </div>
 </template>
 
 <script>
-import HomeHeader from '@/pages/buyer/home/components/Header'
-import HomeSwiper from '@/pages/buyer/home/components/Swiper'
-import HomeIcons from '@/pages/buyer/home/components/Icons'
-import HomeRecommond from '@/pages/buyer/home/components/Recommond'
-import HomeMinimum from '@/pages/buyer/home/components/Minimum'
-import HomeFooter from '@/pages/buyer/home/components/Footer'
+import HomeHeader from '@/pages/seller/home/components/Header'
+import HomeToday from '@/pages/seller/home/components/Today'
+import HomeWeekend from '@/pages/seller/home/components/Weekend'
+import HomeLast from '@/pages/seller/home/components/Last'
+import HomeFooter from '@/pages/seller/home/components/Footer'
 export default {
-  name: 'SellerHome'
+  name: 'SellerHome',
+  data () {
+    return {
+      TodayList: [
+        {
+          id: '0001',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 0,
+          price: 34.1
+        }
+      ],
+      WeekendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 2,
+          price: 34.1
+        }, {
+          id: '0002',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 341,
+          price: 34.02
+        }
+      ],
+      LastList: [
+        {
+          id: '0001',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 21,
+          price: 34.1
+        }, {
+          id: '0002',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 1341,
+          price: 34.02
+        }, {
+          id: '0003',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 2122,
+          price: 34.1
+        }, {
+          id: '0004',
+          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+          name: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+          sale: 111,
+          price: 34.02
+        }
+      ]
+    }
+  },
+  components: {
+    HomeHeader,
+    HomeToday,
+    HomeWeekend,
+    HomeLast,
+    HomeFooter
+  }
 }
 </script>
 
---
+++ src/pages/seller/home/components/Weekend.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="today border">
+    <p class="title border-bottom">一周内上新</p>
+    <ul>
+      <li class="item border-bottom" v-for="item of WeekendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-info">
+          <div class="item-desc">{{item.name}}</div>
+          <div class="item-count">成交量：{{item.sale}}份</div>
+          <div class="item-detail">
+            <div class="item-price">￥{{item.price.toFixed(2)}}</div>
+            <button class="item-button">点击修改</button>
+          </div>
+        </div>
+      </li>
+      <li class="hasNoGoods" :class="{hide: isHide}">暂无上新</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      isHide: true
+    }
+  },
+  props: {
+    WeekendList: Array
+  },
+  mounted () {
+    if (this.WeekendList.length === 0) {
+      this.isHide = !this.isHide
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .today{
+    width: 100%;
+  }
+  .title{
+    display: block;
+    width: 100%;
+    height: .5rem;
+    line-height: .5rem;
+    text-align: center;
+    background-color: #eeeeee;
+    font-size: .34rem;
+    color: #333333;
+  }
+  .item{
+    overflow: hidden;
+    display: flex;
+    height: 1.9rem;
+    /*background-color: red;*/
+  }
+  .item-img{
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: .1rem;
+  }
+  .item-info{
+    flex: 1;
+    padding: .1rem;
+    min-width: 0;
+    /*background-color: green;*/
+  }
+  .item-desc{
+    line-height: .6rem;
+    color: #666666;
+    /*background-color: #9a6e3a;*/
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .item-count{
+    line-height: .5rem;
+    font-size: .28rem;
+    color: #cccccc;
+    /*background-color: yellow;*/
+  }
+  .item-detail{
+    line-height: .6rem;
+    color: #ccc;
+    /*background-color: fuchsia;*/
+  }
+  .item-price{
+    line-height: .44rem;
+    margin-top: .16rem;
+    /*background-color: lightcoral;*/
+    color: red;
+    font-size: .34rem;
+    font-weight: bolder;
+    float: left;
+  }
+  .item-button{
+    line-height: .44rem;
+    margin-top: .16rem;
+    margin-right: .25rem;
+    background-color: #ff9300;
+    padding: 0 .1rem;
+    border-radius: .06rem;
+    color: #fff;
+    float: right;
+  }
+  .hasNoGoods{
+    height: 1rem;
+    line-height: 1rem;
+    width: 100%;
+    text-align: center;
+    font-size: .32rem;
+    color: #333333;
+    background-color: #ffffff;
+  }
+  .hide{
+    display: none;
+  }
+</style>
