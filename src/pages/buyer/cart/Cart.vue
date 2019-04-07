d09d4ab93d7c41225716324b119f49cf268474e2
jinyajuan <2633579745@qq.com>
2019-04-07 15:30:58 +0800

    router-link is finished

--- src/pages/buyer/cart/Cart.vue
+++ src/pages/buyer/cart/Cart.vue
@@ -2,7 +2,6 @@
   <div>
     <cart-header></cart-header>
     <cart-list
-      :CartItemList="CartItemList"
       @countMinusOrAdd="handleCountMinusOrAdd"
       @isSelectSingle="handleIsSelectSingle"
       @deleteGoods="ClickDeleteGoods"
@@ -10,13 +9,14 @@
     <cart-total
       :totalPrice="totalPrice"
       :isSelectAll="isSelectAll"
-      @handleIsSelectAll="handleIsSelectAll"
       @goPay="handleGoPay"
+      @handleIsSelectAll="handleIsSelectAll"
     ></cart-total>
     <home-footer></home-footer>
     <cart-delete-panel
       :isDeleteGoods="isDeleteGoods"
-      @delGood="handleDeleteGood">
+      @delGood="handleDeleteGood"
+    >
     </cart-delete-panel>
     <cart-prompt
       :hasNoGoodsToPay="hasNoGoodsToPay"
@@ -33,52 +33,52 @@ import HomeFooter from '@/pages/buyer/home/components/Footer'
 import CartDeletePanel from '@/pages/buyer/cart/components/Panel'
 import CartPrompt from '@/pages/buyer/cart/components/Prompt'
 export default {
-  name: 'BuyerCart',
+  name: 'Cart',
   data () {
     return {
-      CartItemList: [
-        {
-          id: '0001',
-          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
-          desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
-          count: 1,
-          volume: '12343',
-          price: 1,
-          checked: false
-        }, {
-          id: '0002',
-          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
-          desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
-          count: 1,
-          volume: '12343',
-          price: 2.01,
-          checked: false
-        }, {
-          id: '0003',
-          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
-          desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
-          count: 1,
-          checked: false,
-          volume: '12343',
-          price: 3.00
-        }, {
-          id: '0004',
-          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
-          desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
-          count: 1,
-          volume: '12343',
-          price: 4,
-          checked: false
-        }, {
-          id: '0006',
-          imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
-          desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
-          count: 1,
-          volume: '12343',
-          price: 5,
-          checked: false
-        }
-      ],
+      // CartItemList: [
+      //   {
+      //     id: '0001',
+      //     imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+      //     desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+      //     count: 1,
+      //     volume: '12343',
+      //     price: 1,
+      //     checked: false
+      //   }, {
+      //     id: '0002',
+      //     imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+      //     desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+      //     count: 1,
+      //     volume: '12343',
+      //     price: 2.01,
+      //     checked: false
+      //   }, {
+      //     id: '0003',
+      //     imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+      //     desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+      //     count: 1,
+      //     checked: false,
+      //     volume: '12343',
+      //     price: 3.00
+      //   }, {
+      //     id: '0004',
+      //     imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+      //     desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+      //     count: 1,
+      //     volume: '12343',
+      //     price: 4,
+      //     checked: false
+      //   }, {
+      //     id: '0006',
+      //     imgUrl: 'https://img14.360buyimg.com/babel/s180x180_jfs/t1/15492/22/10575/256417/5c8875d3Ecc98b5ce/d102b1ac792d19a8.jpg!q80.webp',
+      //     desc: '壹农壹果 南非红心西柚 4个 单果250g-320g 葡萄柚 柚子水果 ',
+      //     count: 1,
+      //     volume: '12343',
+      //     price: 5,
+      //     checked: false
+      //   }
+      // ],
       isSelectAll: false,
       totalPrice: 0,
       isDeleteGoods: true,
@@ -91,18 +91,18 @@ export default {
     CartList,
     CartTotal,
     HomeFooter,
-    CartDeletePanel,
-    CartPrompt
+    CartPrompt,
+    CartDeletePanel
   },
   methods: {
     // 1.单个商品的加减
     handleCountMinusOrAdd (flag, index) {
       // 1.1 商品数量的加减
       if (flag) { // 加
-        this.CartItemList[index].count++
+        this.$store.state.CartItemList[index].count++
       } else { // 减
-        if (this.CartItemList[index].count > 1) {
-          this.CartItemList[index].count--
+        if (this.$store.state.CartItemList[index].count > 1) {
+          this.$store.state.CartItemList[index].count--
         }
       }
       // 1.2  计算总价
@@ -111,7 +111,7 @@ export default {
     // 2.单个商品的选中
     handleIsSelectSingle (flag, index) {
       // 2.1 商品选中或者取消
-      this.CartItemList[index].checked = !flag
+      this.$store.state.CartItemList[index].checked = !flag
       // 2.2计算总价
       this.handleAllPrice()
       //  2.3
@@ -122,8 +122,8 @@ export default {
       // 3.1 全选按钮选中护着取消
       this.isSelectAll = flag
       // 3.2 全选按钮选中或者取消时对每个单选按钮的影响
-      this.CartItemList.forEach((item, index) => {
-        this.CartItemList[index].checked = flag
+      this.$store.state.CartItemList.forEach((item, index) => {
+        this.$store.state.CartItemList[index].checked = flag
       })
       // 3.3 计算总价
       this.handleAllPrice()
@@ -132,7 +132,7 @@ export default {
     handleAllPrice () {
       let totalPrice = 0
       // 遍历每一项，当选中的状态下进行计算
-      this.CartItemList.forEach((item) => {
+      this.$store.state.CartItemList.forEach((item) => {
         if (item.checked) {
           totalPrice += (parseInt(item.count) * (parseFloat(item.price).toFixed(2)))
         }
@@ -142,7 +142,7 @@ export default {
     // 5 当所有的商品全部逐一选中时，全选按钮显示选中
     selectAll () {
       let selectFlag = true
-      this.CartItemList.forEach((item) => {
+      this.$store.state.CartItemList.forEach((item) => {
         if (!item.checked) {
           selectFlag = false
         }
@@ -161,22 +161,32 @@ export default {
         this.isDeleteGoods = true
         //  7.2 删除对应的数组部分
         let index = this.currentDeleteGoodIndex
-        this.CartItemList.splice(index, 1)
+        this.$store.state.CartItemList.splice(index, 1)
         // 7.3 计算总价
         this.handleAllPrice()
       } else { // 取消删除
         this.isDeleteGoods = true
       }
     },
-    //  8.处理付款
+    //  8.付款时的跳转
     handleGoPay () {
-      if (this.totalPrice === 0) {
+      // 8.1 遍历购物车列表，将选中状态下的商品添加到CartItemListGoPay
+      this.$store.state.CartItemList.forEach((item) => {
+        if (item.checked) {
+          this.$store.state.CartItemListGoPay.push(item)
+        }
+      })
+
+      console.log(this.$store.state.CartItemList)
+      console.log(this.$store.state.CartItemListGoPay)
+      // 8.2 判断CartItemListGoPay的长度，当长度为0时，弹出提示，反之，跳转
+      if (this.$store.state.CartItemListGoPay.length === 0) {
         this.hasNoGoodsToPay = false
       } else {
-        this.$router.push('/buyer/order')
+        this.$router.push(path: '/buyer/order)
       }
     },
-    // 9.点击确定的时候去除面板
+    //  9. 处理弹出面板的消失
     handleDelNoGoodsToPay () {
       this.hasNoGoodsToPay = true
     }
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    CartItemList: []
+    CartItemList: [],
+    CartItemListGoPay: []
   }
 })
