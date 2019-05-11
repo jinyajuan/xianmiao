6d6bdb003ecbe497175a81c7462ccc123e3af124
jinyajuan <2633579745@qq.com>
2019-05-11 15:35:47 +0800

    buyer reg info check

--- src/pages/buyer/reg/components/Center.vue
+++ src/pages/buyer/reg/components/Center.vue
@@ -3,7 +3,7 @@
       <div>
         <div :class="{'hide': isHide}">
           <input class="border-bottom" type="text" placeholder="请输入用户名" v-model="buyer_id">
-          <input class="border-bottom" type="password" placeholder="请输入密码" v-model="buyer_pwd">
+          <input class="border-bottom" type="password" placeholder="请输入至少8位密码" v-model="buyer_pwd">
           <input class="border-bottom" type="password" placeholder="请再次输入密码" v-model="buyer_pwd_again">
           <input class="btn" type="button" @click="handleNext" value="下一步">
         </div>
@@ -34,24 +34,57 @@ export default {
   },
   methods: {
     handleNext () {
-      this.isHide = !this.isHide
+      if (this.buyer_id.length === 0) {
+        alert('用户名不能为空！')
+      } else if (this.buyer_pwd.length === 0) {
+        alert('密码不能为空')
+      } else if (this.buyer_pwd_again.length === 0) {
+        alert('确认密码不能为空')
+      } else if (this.buyer_pwd_again !== this.buyer_pwd) {
+        alert('密码输入不一致，请重新输入！')
+        this.buyer_pwd_again = ''
+        this.buyer_pwd = ''
+      } else if (this.buyer_pwd.length < 8) {
+        alert('密码长度不能少于8位')
+      } else {
+        this.isHide = !this.isHide
+      }
     },
     buyerReg () {
-      axios.post('/buyer/reg', {
-        buyer_id: this.buyer_id,
-        buyer_pwd: this.buyer_pwd,
-        buyer_phone: this.buyer_phone,
-        buyer_name: this.buyer_name,
-        buyer_address: this.buyer_address
-      }).then((response) => {
-        let res = response.data
-        if (res.status === 0) {
-          alert(res.msg)
-          this.$router.push('/buyer/login')
-        } else {
-          alert(res.msg)
-        }
-      })
+      if (this.buyer_name.length === 0) {
+        alert('收货人姓名不能为空！')
+      } else if (this.buyer_phone.length === 0) {
+        alert('收货人联系方式不能为空！')
+      } else if (this.buyer_address.length === 0) {
+        alert('收货人地址不能为空！')
+      } else if (!this.buyer_phone.match(/^[1][3|4|5|8][0-9]{9}$/)) {
+        alert('手机号码格式有误，请重新输入~')
+        this.buyer_phone = ''
+      } else {
+        axios.post('/buyer/reg', {
+          buyer_id: this.buyer_id,
+          buyer_pwd: this.buyer_pwd,
+          buyer_phone: this.buyer_phone,
+          buyer_name: this.buyer_name,
+          buyer_address: this.buyer_address
+        }).then((response) => {
+          let res = response.data
+          if (res.status === 0) {
+            alert(res.msg)
+            this.$router.push('/buyer/login')
+          } else {
+            alert(res.msg)
+            this.isHide = !this.isHide
+            this.$router.push('/buyer/reg')
+            this.buyer_id = ''
+            this.buyer_pwd = ''
+            this.buyer_pwd_again = ''
+            this.buyer_name = ''
+            this.buyer_phone = ''
+            this.buyer_address = ''
+          }
+        })
+      }
     }
   }
 }
